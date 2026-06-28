bcf5cd46ace64e193f2fad31a17a66fc07cd09de
Oduh Emmanuel Aba <eoduh22@gmail.com>
2026-06-28 15:46:18 +0000

    undate in changes

--- main.go
+++ main.go
@@ -5,50 +5,50 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
+// page layout for a wiki page
 type Page struct {
 	Title string
 	Body  []byte
 }
-
+// method to save on the page
 func (p *Page) save() error {
+	// use title as file name to write
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
-
+// read file to load the page  
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	bodyData, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: filename, Body: bodyData}, nil
+	return &Page{Title: title, Body: bodyData}, nil
 }
-
-func veiwHandler(w http.ResponseWriter, r *http.Request) {
+// vieww handler for viewing a page
+func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-	p = &Page{Title: title}
 	RenderTemplat(w, "view.html", p)
 }
-
+// handler to edit a page
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
 	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
+		p = &Page{Title: title}
 	}
-	page := p.Title
-	RenderTemplat(w, "edit.html", page)
+	RenderTemplat(w, "edit.html", p)
 }
-
+// helper function to render response template
 func RenderTemplat(w http.ResponseWriter, file string, data any) {
 	err := tmpl.ExecuteTemplate(w, "templates/"+file, data)
 	if err != nil {
@@ -56,14 +56,21 @@ func RenderTemplat(w http.ResponseWriter, file string, data any) {
 		return
 	}
 }
+// handle saving of pages
 func SaveHandle(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	page := &Page{Title: title, Body: []byte(body)}
+	page.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-var tmpl template.Template
+var tmpl *template.Template
 
 func main() {
-	tmpl = *template.Must(template.ParseFiles("edit.html"))
-	http.HandleFunc("/save", SaveHandle)
+	tmpl = template.Must(template.ParseFiles("edit.html"))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/save/", SaveHandle)
 	http.HandleFunc("/edit/", editHandler)
 	fmt.Println("starting sever on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
