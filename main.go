9d1321dfd0c78d0e923f2a5990159baf9a458ecc
Oduh Emmanuel Aba <eoduh22@gmail.com>
2026-06-29 06:13:37 +0000

    project overview complete

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -34,11 +33,7 @@ func loadPage(title string) (*Page, error) {
 }
 
 // vieww handler for viewing a page
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -48,11 +43,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // handler to edit a page
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -70,24 +61,31 @@ func RenderTemplat(w http.ResponseWriter, file string, data any) {
 }
 
 // handle saving of pages
-func SaveHandle(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func SaveHandle(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	page := &Page{Title: title, Body: []byte(body)}
 	page.save()
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	sm := validPath.FindStringSubmatch(r.URL.Path)
-	if sm == nil {
-		http.NotFound(w, r)
-		return "", errors.New("Invalide title page")
+// func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+// 	sm := validPath.FindStringSubmatch(r.URL.Path)
+// 	if sm == nil {
+// 		http.NotFound(w, r)
+// 		return "", errors.New("Invalide title page")
+// 	}
+// 	return sm[2], nil
+// }
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
 	}
-	return sm[1], nil
 }
 
 var tmpl *template.Template
@@ -96,9 +94,9 @@ var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func main() {
 	tmpl = template.Must(template.ParseFiles("edit.html"))
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/save/", SaveHandle)
-	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/save/", makeHandler(SaveHandle))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
 	fmt.Println("starting sever on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
